1d9f93b3d297a40068f14dfe97cbd03cf3b0a858
tess.aquilon <tess.aquilon@gmail.com>
2023-03-29 15:30:00 +0200

    added useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 export const App = () => {
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://pokeapi.co/api/v2/pokemon')
+    .then((res) => res.json())
+    .then((data) => {
+      setList(data.results)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+    .finally(() => {
+      setLoading(false);
+    })
+  }, []);
   return (
     <div>
       Find me in src/app.js!
